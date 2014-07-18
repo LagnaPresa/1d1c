0970e537cd7638f440991391c4c0414e255c5570
LagnaPresa <LagnaPresaOnGitHub@gmail.com>
2014-07-19 00:33:19 +0900

    daily

---
+++ 20140718/GreedyLanguage.groovy
@@ -0,0 +1,10 @@
+package net.n39i.ltx1d1c.d20140718
+
+class GreedyLanguage {
+
+    public static void main(String[] args) {
+        println([1,2,3].subsequences().sort())
+        // [[1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]
+    }
+
+}
