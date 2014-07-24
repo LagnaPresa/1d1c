c49b5e47ff6ac9ab28318a9b12e026f601e883b5
LagnaPresa <LagnaPresaOnGitHub@gmail.com>
2014-07-24 23:31:26 +0900

    daily

---
+++ 20140724/LongJoke.java
@@ -0,0 +1,14 @@
+package net.n39i.ltx1d1c.d140724;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LongJoke {
+
+	@Test
+	public void testYourMind() {
+		assertEquals(802, (1234 - 432l));
+	}
+
+}
